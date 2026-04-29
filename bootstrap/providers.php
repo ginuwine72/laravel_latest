618513f2971e1924deab851850987c59689beca5
Pechkul Yuenyong <11808196+ginuwine72@users.noreply.github.com>
2026-04-29 15:18:49 +0700

    added resend/laravel, laravel/horizon
    added resend/laravel, laravel/horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -7,4 +7,5 @@ return [
     App\Providers\Filament\PlatformPanelProvider::class,
     App\Providers\Filament\WebsitePanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
